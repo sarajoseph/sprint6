10d9c71c4ba5d7c15bf694501eca9db2b8e53f76
Sara <sarajosephperez@gmail.com>
2024-07-13 22:25:42 +0200

    react router updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 /* eslint-disable react/react-in-jsx-scope */
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Calculator } from './pages/Calculator'
+import { Home } from './pages/Home'
+import { BudgetUrl } from './pages/BudgetUrl'
+import { NotFound } from './pages/NotFound'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />
+  },
+  {
+    path: '/calculator',
+    element: <Calculator />
+  },
+  {
+    path: '/budget/:params',
+    element: <BudgetUrl />
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <RouterProvider router={router} />
 )
